1f03a411b2c7ea25df1634bf5d6265c663494c76
AndreBarajas <bvandrea7@gmail.com>
2022-08-05 13:23:56 -0500

    Agrega las imagenes a las cuadriculas

--- src/main.js
+++ src/main.js
@@ -1,6 +1,50 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import allData from "./data/pokemon/pokemon.js"
 
-console.log(example, data);
+const mainData = allData.pokemon;
+
+const nombresPokemon = mainData.map(
+    (pokemon) => pokemon.name
+); console.log(nombresPokemon);
+
+const categoriaPokemon = mainData.map(
+    (pokemon) => pokemon.type
+); console.log(categoriaPokemon);
+
+
+const pokemones = mainData.map(
+    (pokemon) => ({num: pokemon.num, name: pokemon.name, type: pokemon.type, img: pokemon.img})
+); console.log(pokemones);
+
+
+const pokemonCard = (mainData) => {
+    let pokemonContainer = document.getElementById("containerPokemon");
+    let displayPokemon = document.createElement("div");
+    displayPokemon.className = 'pokeDiv';
+    pokemonContainer.appendChild(displayPokemon);
+    console.log(displayPokemon);
+    
+    let displayImg = document.createElement("img");
+    displayImg.src = mainData.img;
+    displayImg.className = 'pokeImg';
+    displayPokemon.appendChild(displayImg)
+       
+}; 
+
+mainData.map(currentPokemon => {
+    pokemonCard(currentPokemon);
+
+});
+
+
+
+//function () {
+//    for (let i = 0; i < 252; i++) {
+//        infoGrid(i)
+//        console.log(infoGrid);
+//    }
+
+
+
+
+
+
