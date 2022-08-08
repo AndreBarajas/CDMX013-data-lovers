4c8248001682c0efb55682401aed044e3fcd92ef
AndreBarajas <bvandrea7@gmail.com>
2022-08-08 11:36:09 -0500

    Deja visualizar a los pokemos

--- src/main.js
+++ src/main.js
@@ -2,20 +2,6 @@ import allData from "./data/pokemon/pokemon.js"
 
 const mainData = allData.pokemon;
 
-const nombresPokemon = mainData.map(
-    (pokemon) => pokemon.name
-); console.log(nombresPokemon);
-
-const categoriaPokemon = mainData.map(
-    (pokemon) => pokemon.type
-); console.log(categoriaPokemon);
-
-
-const pokemones = mainData.map(
-    (pokemon) => ({num: pokemon.num, name: pokemon.name, type: pokemon.type, img: pokemon.img})
-); console.log(pokemones);
-
-
 const pokemonCard = (mainData) => {
     let pokemonContainer = document.getElementById("containerPokemon");
     let displayPokemon = document.createElement("div");
@@ -24,27 +10,25 @@ const pokemonCard = (mainData) => {
     console.log(displayPokemon);
     
     let displayImg = document.createElement("img");
-    displayImg.src = mainData.img;
     displayImg.className = 'pokeImg';
+    displayImg.src = mainData.img;
     displayPokemon.appendChild(displayImg)
-       
-}; 
+
+    let pokemonBasicInfo = document.createElement("div");
+    pokemonBasicInfo.className = 'pokemonInfo';
+    let pokemonNumName = document.createElement("h3");
+    pokemonNumName.innerHTML = "#" + mainData.num + " " + mainData.name;
+    pokemonBasicInfo.appendChild(pokemonNumName);
+    displayPokemon.appendChild(pokemonBasicInfo);
+
+    let type = document.createElement("h4");
+    type.className = 'pokemonType';
+    let pokemonTypes = mainData.type;
+    type.innerHTML = pokemonTypes;
+    displayPokemon.appendChild(type);
+    }; 
 
 mainData.map(currentPokemon => {
     pokemonCard(currentPokemon);
 
 });
-
-
-
-//function () {
-//    for (let i = 0; i < 252; i++) {
-//        infoGrid(i)
-//        console.log(infoGrid);
-//    }
-
-
-
-
-
-
