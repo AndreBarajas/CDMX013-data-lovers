0cf8307307f159cbf08b8837d52c7450d3d00806
Andrea Barajas <108833574+AndreBarajas@users.noreply.github.com>
2022-08-17 17:37:46 -0500

    Agrega la función de sort

--- src/data.js
+++ src/data.js
@@ -1,43 +1,66 @@
-export const filterGen = ( mainData,selectedGeneration) => {
+
+export const filterGen = (mainData, selectedGeneration) => {
   const genPokemon = mainData.filter(mainData => {
     if (mainData.generation.name == (selectedGeneration)) {
       return true;
-    }})
-    console.log(genPokemon);
-  if (genPokemon.length === 0){
-    return mainData;}
-    else {return genPokemon;}  
-}; 
+    }
+  })
+  console.log(genPokemon);
+  if (genPokemon.length === 0) {
+    return mainData;
+  }
+  else { return genPokemon; }
+};
 export const filterType = (mainData, selectedType) => {
   const typePokemon = mainData.filter(mainData => {
     if (mainData.type.includes(selectedType)) {
       return true;
-    }})
-    console.log(typePokemon);
-    if (typePokemon.length === 0){
-      return mainData;}
-      else {return typePokemon;}
-    };
+    }
+  })
+  console.log(typePokemon);
+  if (typePokemon.length === 0) {
+    return mainData;
+  }
+  else { return typePokemon; }
+};
 
 export const filterWeakness = (mainData, selectedWeakness) => {
   const weaknessPokemon = mainData.filter(mainData => {
     if (mainData.weaknesses.includes(selectedWeakness)) {
       return true;
-    }})
-    console.log(weaknessPokemon);
-    if (weaknessPokemon.length === 0){
-      return mainData;}
-      else {return weaknessPokemon;}
-    };
+    }
+  })
+  console.log(weaknessPokemon);
+  if (weaknessPokemon.length === 0) {
+    return mainData;
+  }
+  else { return weaknessPokemon; }
+};
 
 export const filterRarity = (mainData, selectedRarity) => {
   const resultRarity = mainData.filter(mainData => {
     if (mainData["pokemon-rarity"] == (selectedRarity)) {
       return true;
-    }}) 
-      console.log(resultRarity);
-    if (resultRarity.length === 0){
-      return mainData;}
-      else {return resultRarity;}
-    };
-   
+    }
+  })
+  console.log(resultRarity);
+  if (resultRarity.length === 0) {
+    return mainData;
+  }
+  else { return resultRarity; }
+};
+
+export const filterOrder = (mainData, sortOrder) => {
+  const resultOrder = mainData.sort((a, b) => {
+    if (sortOrder == 1) {
+      return a.name > b.name
+    }
+    if (sortOrder == -1) {
+      return a.name < b.name;
+    }
+    else {
+      return mainData;
+    }
+  })
+  return resultOrder;
+};
--- src/main.js
+++ src/main.js
@@ -1,36 +1,37 @@
+
 import allData from "./data/pokemon/pokemon.js"
-import {filterType, filterGen, filterWeakness, filterRarity} from "./data.js"
+import { filterType, filterGen, filterWeakness, filterRarity, filterOrder } from "./data.js"
 
 const mainData = allData.pokemon;
 let pokeData = allData.pokemon;
 
 export const pokemonCard = (mainData) => {
-    let pokemonContainer = document.getElementById("containerPokemon");
-    let displayPokemon = document.createElement("div");
-    displayPokemon.className = 'pokeDiv';
-    pokemonContainer.appendChild(displayPokemon);
-         
-    let displayImg = document.createElement("img");
-    displayImg.className = 'pokeImg';
-    displayImg.src = mainData.img;
-    displayPokemon.appendChild(displayImg)
+  let pokemonContainer = document.getElementById("containerPokemon");
+  let displayPokemon = document.createElement("div");
+  displayPokemon.className = 'pokeDiv';
+  pokemonContainer.appendChild(displayPokemon);
 
-    let pokemonBasicInfo = document.createElement("div");
-    pokemonBasicInfo.className = 'pokemonInfo';
-    let pokemonNumName = document.createElement("h3");
-    pokemonNumName.innerHTML = "#" + mainData.num + " " + mainData.name;
-    pokemonBasicInfo.appendChild(pokemonNumName);
-    displayPokemon.appendChild(pokemonBasicInfo);
+  let displayImg = document.createElement("img");
+  displayImg.className = 'pokeImg';
+  displayImg.src = mainData.img;
+  displayPokemon.appendChild(displayImg)
 
-    let type = document.createElement("h4");
-    type.className = 'pokemonType';
-    let pokemonTypes = mainData.type;
-    type.innerHTML = pokemonTypes;
-    displayPokemon.appendChild(type);
-    }; 
+  let pokemonBasicInfo = document.createElement("div");
+  pokemonBasicInfo.className = 'pokemonInfo';
+  let pokemonNumName = document.createElement("h3");
+  pokemonNumName.innerHTML = "#" + mainData.num + " " + mainData.name;
+  pokemonBasicInfo.appendChild(pokemonNumName);
+  displayPokemon.appendChild(pokemonBasicInfo);
+
+  let type = document.createElement("h4");
+  type.className = 'pokemonType';
+  let pokemonTypes = mainData.type;
+  type.innerHTML = pokemonTypes;
+  displayPokemon.appendChild(type);
+};
 
 mainData.map(currentPokemon => {
-    pokemonCard(currentPokemon);
+  pokemonCard(currentPokemon);
 
 });
 
@@ -39,39 +40,49 @@ let selectGen = document.getElementById("generation");
 // e = evento, target= se refiere al input, .value nos da el valor del input
 selectGen.addEventListener("change", (e) => {
   let selectedGeneration = e.target.value;
-  pokeData = filterGen(mainData,selectedGeneration);
-    document.getElementById("containerPokemon").innerHTML = "";
-    pokeData.forEach(currentPokemon => {
-      pokemonCard(currentPokemon);
-    });
-}); 
+  pokeData = filterGen(mainData, selectedGeneration);
+  document.getElementById("containerPokemon").innerHTML = "";
+  pokeData.forEach(currentPokemon => {
+    pokemonCard(currentPokemon);
+  });
+});
 
 let selectType = document.getElementById("type");
 selectType.addEventListener("change", (e) => {
   let selectedType = e.target.value;
-  pokeData = filterType(mainData,selectedType);
-    document.getElementById("containerPokemon").innerHTML = "";
-    pokeData.forEach(currentPokemon => {
-      pokemonCard(currentPokemon);
-    });
+  pokeData = filterType(mainData, selectedType);
+  document.getElementById("containerPokemon").innerHTML = "";
+  pokeData.forEach(currentPokemon => {
+    pokemonCard(currentPokemon);
+  });
 });
 
 let selectWeakness = document.getElementById("weaknessPokemon");
-     selectWeakness.addEventListener("change", (e) => {
-         let selectedWeakness = e.target.value;
-         pokeData = filterWeakness(mainData, selectedWeakness);
-         document.getElementById("containerPokemon").innerHTML = "";
-         pokeData.forEach(currentPokemon => {
-             pokemonCard(currentPokemon)
-         });
-     });
-     
-     let selectRarity = document.getElementById("pokemonRarity");
+selectWeakness.addEventListener("change", (e) => {
+  let selectedWeakness = e.target.value;
+  pokeData = filterWeakness(mainData, selectedWeakness);
+  document.getElementById("containerPokemon").innerHTML = "";
+  pokeData.forEach(currentPokemon => {
+    pokemonCard(currentPokemon)
+  });
+});
+
+let selectRarity = document.getElementById("pokemonRarity");
 selectRarity.addEventListener("change", (e) => {
-    let selectedRarity = e.target.value;
-    pokeData = filterRarity(mainData, selectedRarity);
-    document.getElementById("containerPokemon").innerHTML = "";
-    pokeData.forEach(currentPokemon => {
-             pokemonCard(currentPokemon)
-         });
-     });
+  let selectedRarity = e.target.value;
+  pokeData = filterRarity(mainData, selectedRarity);
+  document.getElementById("containerPokemon").innerHTML = "";
+  pokeData.forEach(currentPokemon => {
+    pokemonCard(currentPokemon)
+  });
+});
+
+let selectOrder = document.getElementById("sortMenu");
+selectOrder.addEventListener("change", (e) => {
+  let sortOrder = e.target.value;
+  pokeData = filterOrder (pokeData, sortOrder);
+  document.getElementById("containerPokemon").innerHTML = "";
+  pokeData.forEach(currentPokemon => {
+    pokemonCard(currentPokemon)
+  });
+});
