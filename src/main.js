cf4daf4cc565152b72f6805d10d043e7620013c0
DianaAmpudia <dianalauraampudia@gmail.com>
2022-08-19 18:56:30 -0500

    cambios

--- src/main.js
+++ src/main.js
@@ -34,75 +34,118 @@ mainData.map(currentPokemon => {
 
 });
 
-//FILTROS
+// //FILTROS
 
+// let selectGen = document.getElementById("generation");
+// let selectType = document.getElementById("type");
+// let selectWeakness = document.getElementById("weaknessPokemon");
+// let selectRarity = document.getElementById("pokemonRarity");
+// let selectOrder = document.getElementById("sortMenu");
+
+
+// //FILTRO GENERACIÓN/REGIÓN 
+
+// selectGen.addEventListener("change", (e) => {
+//   let selectedGeneration = e.target.value;
+//   let selectedType = selectType.value;
+//   let selectedWeakness = selectWeakness.value;
+//   let selectedRarity = selectRarity.value;
+
+//   pokeData = allFilters(mainData,selectedGeneration, selectedType, selectedWeakness, selectedRarity);
+//   document.getElementById("containerPokemon").innerHTML = "";
+//   pokeData.forEach(currentPokemon => {
+//     pokemonCard(currentPokemon);
+//   });
+// });
+
+//FILTRO TIPO DE POKEMON
+
+// selectType.addEventListener("change", (e) => {
+//   let selectedType = e.target.value;
+//   let selectedGeneration = selectGen.value;
+//   let selectedWeakness = selectWeakness.value;
+//   let selectedRarity = selectRarity.value;
+
+//   pokeData = allFilters(mainData,selectedGeneration, selectedType, selectedWeakness, selectedRarity);
+//   document.getElementById("containerPokemon").innerHTML = "";
+//   pokeData.forEach(currentPokemon => {
+//     pokemonCard(currentPokemon);
+//   });
+// });
+
+// //FILTRO DEBILIDAD
+
+// selectWeakness.addEventListener("change", (e) => {
+//   let selectedWeakness = e.target.value;
+//   let selectedGeneration = selectGen.value;
+//   let selectedType = selectType.value;
+//   let selectedRarity = selectRarity.value;
+
+//   pokeData = allFilters(mainData,selectedGeneration, selectedType, selectedWeakness, selectedRarity);
+//   document.getElementById("containerPokemon").innerHTML = "";
+//   pokeData.forEach(currentPokemon => {
+//     pokemonCard(currentPokemon)
+//   });
+// });
+
+
+// //FILTRO RAREZA
+
+// selectRarity.addEventListener("change", (e) => {
+//   let selectedRarity = e.target.value;
+//   let selectedGeneration = selectGen.value;
+//   let selectedType = selectType.value;
+//   let selectedWeakness = selectWeakness.value;
+
+//   pokeData = allFilters(mainData, selectedGeneration, selectedType, selectedWeakness, selectedRarity);
+//   document.getElementById("containerPokemon").innerHTML = "";
+//   pokeData.forEach(currentPokemon => {
+//     pokemonCard(currentPokemon)
+//   });
+// });
+
+// //ORDENAR DE A-Z Y Z-A
+
+// selectOrder.addEventListener("change", (e) => {
+//   let sortOrder = e.target.value;
+//   pokeData = filterOrder(pokeData, sortOrder);
+//   document.getElementById("containerPokemon").innerHTML = "";
+//   pokeData.forEach(currentPokemon => {
+//     pokemonCard(currentPokemon)
+//   });
+// });
+
+// /*const filters ={
+//   gen:valor
+//   type:Valor
+// }*/
+
+//FILTROS 
 let selectGen = document.getElementById("generation");
 let selectType = document.getElementById("type");
 let selectWeakness = document.getElementById("weaknessPokemon");
 let selectRarity = document.getElementById("pokemonRarity");
 let selectOrder = document.getElementById("sortMenu");
 
-
-//FILTRO GENERACIÓN/REGIÓN 
-
-selectGen.addEventListener("change", (e) => {
-  let selectedGeneration = e.target.value;
-  let selectedType = selectType.value;
-  let selectedWeakness = selectWeakness.value;
-  let selectedRarity = selectRarity.value;
-
-  pokeData = allFilters(mainData,selectedGeneration, selectedType, selectedWeakness, selectedRarity);
-  document.getElementById("containerPokemon").innerHTML = "";
-  pokeData.forEach(currentPokemon => {
-    pokemonCard(currentPokemon);
-  });
-});
-
-//FILTRO TIPO DE POKEMON
-
-selectType.addEventListener("change", (e) => {
-  let selectedType = e.target.value;
-  let selectedGeneration = selectGen.value;
-  let selectedWeakness = selectWeakness.value;
-  let selectedRarity = selectRarity.value;
-
-  pokeData = allFilters(mainData,selectedGeneration, selectedType, selectedWeakness, selectedRarity);
-  document.getElementById("containerPokemon").innerHTML = "";
-  pokeData.forEach(currentPokemon => {
-    pokemonCard(currentPokemon);
-  });
-});
-
-//FILTRO DEBILIDAD
-
-selectWeakness.addEventListener("change", (e) => {
-  let selectedWeakness = e.target.value;
-  let selectedGeneration = selectGen.value;
-  let selectedType = selectType.value;
-  let selectedRarity = selectRarity.value;
-
-  pokeData = allFilters(mainData,selectedGeneration, selectedType, selectedWeakness, selectedRarity);
-  document.getElementById("containerPokemon").innerHTML = "";
-  pokeData.forEach(currentPokemon => {
-    pokemonCard(currentPokemon)
-  });
-});
-
-
-//FILTRO RAREZA
-
-selectRarity.addEventListener("change", (e) => {
-  let selectedRarity = e.target.value;
-  let selectedGeneration = selectGen.value;
-  let selectedType = selectType.value;
-  let selectedWeakness = selectWeakness.value;
+const Onchange  = () => {
+  let selectedRarity = document.getElementById("pokemonRarity").value;
+  let selectedGeneration = document.getElementById("generation").value;
+  let selectedType = document.getElementById("type").value;
+  let selectedWeakness = document.getElementById("weaknessPokemon").value;
 
   pokeData = allFilters(mainData, selectedGeneration, selectedType, selectedWeakness, selectedRarity);
   document.getElementById("containerPokemon").innerHTML = "";
   pokeData.forEach(currentPokemon => {
     pokemonCard(currentPokemon)
-  });
-});
+  })};
+
+selectGen.addEventListener("change", Onchange);
+
+selectType.addEventListener("change", Onchange);
+
+selectWeakness.addEventListener("change", Onchange);
+
+selectRarity.addEventListener("change", Onchange);
 
 //ORDENAR DE A-Z Y Z-A
 
